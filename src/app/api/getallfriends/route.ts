2237243e9acaf894dfe1e1470693c4f4e32784c1
Shishir <skjskjskj333@gmail.com>
2024-03-23 23:22:51 +0530

    prerendering error problem duringdeployment

--- src/app/api/getallfriends/route.ts
+++ src/app/api/getallfriends/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/database/mongoose";
 import { auth } from "@clerk/nextjs";
